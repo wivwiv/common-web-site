f54e93411b67b93f073669b753b34962db69cae7
wivwiv <bigpar@qq.com>
2020-03-19 15:35:33 +0800

    Init API

--- api/info.ts
+++ api/info.ts
@@ -1,8 +1,6 @@
 import { NowRequest, NowResponse } from '@now/node'
-import axios from 'axios';
 import * as fs from 'fs'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const resp = await axios.get('http://ip-api.com/json')
-  res.json({ ip: resp.data, d: fs.readdirSync('/') });
+  res.json({ ip: 0, d: fs.readdirSync('/') });
 }
