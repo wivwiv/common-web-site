5e4c9d5bf6df482d30dd27227a21fcfee266afab
wivwiv <bigpar@qq.com>
2020-03-27 18:14:23 +0800

    Add enterprise

--- api/info.ts
+++ api/info.ts
@@ -1,6 +1,23 @@
 import { NowRequest, NowResponse } from '@now/node'
-import * as fs from 'fs'
+
+import mysql from 'mysql'
+
+mysql.connect({
+  host: 'localhost',
+  username: 'root',
+  password: '123456',
+  database: 'mqtt',
+  pool: 8,
+})
 
 export default async (req: NowRequest, res: NowResponse) => {
-  res.json({ ip: 0, d: fs.readdirSync('/') });
+  const { clientid, password, payload, topic } = req.query;
+
+  // 查询
+  let client = mysql.query('select password from clients where clientid = ?', clientid);
+  const authResult = client ? client.password === clientid : false;
+
+  res.json({
+    result: authResult,
+  });
 }
